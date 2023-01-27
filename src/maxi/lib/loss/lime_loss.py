4d9a1c8e3b11a2fc29f56d6f13b9959a1c09aaf2
tuananhroman <anh.maxiking@hotmail.de>
2023-01-27 12:55:57 +0100

    lime comment

--- src/maxi/lib/loss/lime_loss.py
+++ src/maxi/lib/loss/lime_loss.py
@@ -24,7 +24,6 @@ class LimeLoss(BaseExplanationModel):
         USRVGradientEstimator,
     ]
     _x0_generator = lambda x: np.full(x.shape, 0)
-    # _x0_generator = lambda x: x
 
     def __init__(
         self,
@@ -33,12 +32,32 @@ class LimeLoss(BaseExplanationModel):
         n_samples: int,
         lower: np.ndarray = None,
         upper: np.ndarray = None,
+        channels_first: bool = True,
         *args,
         **kwargs,
     ):
+        """Loss function for LIME (Local Interpretable Model-Agnostic Explanations)
+
+        For the theoretical background refer to:
+            https://arxiv.org/abs/1602.04938
+
+        Args:
+            org_img (np.ndarray): Original image [width, height, channels] or [channels, width, height].
+            inference (InferenceCall): Inference method of an external prediction entity. Has to return an \
+                interpretable representation of the underlying prediction, e.g. a binary vector indicating \
+                the “presence” or “absence”.
+            n_samples (int): Number of samples to draw from the perturbation distribution.
+            lower (np.ndarray, optional): Lower bound for the optimization. Has to be of the same shape as the \
+                target image. Defaults to None.
+            upper (np.ndarray, optional): Upper bound for the optimization. Has to be of the same shape as the \
+                target image. Defaults to None.
+                
+        Configurable Parameters:
+            n_samples
+        """
         if not hasattr(self, "superpixel_handler"):
             self._init_lower_upper(lower, upper, org_img)
-            self._valid_check_lower_upper(org_img)
+            self._valid_check_lower_upper(self._lower, self._upper, org_img)
 
         super().__init__(
             org_img=org_img,
@@ -48,6 +67,7 @@ class LimeLoss(BaseExplanationModel):
             upper=self._upper,
         )
         self.n_samples = n_samples
+        self.channels_first = channels_first
         if not hasattr(self, "superpixel_handler"):
             self._setup_loss_constants()
 
@@ -57,53 +77,99 @@ class LimeLoss(BaseExplanationModel):
         upper: Union[None, np.ndarray],
         org_img: np.ndarray,
     ):
+        """Initializes the lower and upper bound for the optimization.
+
+        Args:
+            lower (Union[None, np.ndarray]): Lower bound for the optimization.
+            upper (Union[None, np.ndarray]): Upper bound for the optimization.
+            org_img (np.ndarray): Original image.
+        """
         DEFAULT_LB_UB = {
-            "lower": np.full(org_img.shape, -5000.0, np.float32),
+            "lower": np.full(org_img.shape, -1.0, np.float32),
             # "lower": np.zeros(org_img.shape),
             # "upper": to_numpy(org_img),
-            "upper": np.full(org_img.shape, 5000.0, np.float32),
+            "upper": np.full(org_img.shape, 1.0, np.float32),
         }
 
         self._lower = DEFAULT_LB_UB["lower"] if lower is None else lower
         self._upper = DEFAULT_LB_UB["upper"] if upper is None else upper
 
-    def _valid_check_lower_upper(self, org_img: np.ndarray) -> None:
-        assert (
-            type(self._lower) is np.ndarray
-        ), "Invalid lower bound given for optimization"
-        assert (
-            type(self._upper) is np.ndarray
-        ), "Invalid upper bound given for optimization"
+    def _valid_check_lower_upper(
+        self,
+        lower: Union[None, np.ndarray],
+        upper: Union[None, np.ndarray],
+        org_img: np.ndarray,
+    ) -> None:
+        """Validates the lower and upper bound for the optimization.
+
+        Args:
+            lower (Union[None, np.ndarray]): Lower bound for the optimization.
+            upper (Union[None, np.ndarray]): Upper bound for the optimization.
+            org_img (np.ndarray): Original image.
+
+        Raises:
+            ValueError: Lower bound matrix of different shape than the input image.
+            ValueError: Upper bound matrix of different shape than the input image.
+        """
+        assert type(lower) is np.ndarray, "Invalid lower bound given for optimization"
+        assert type(upper) is np.ndarray, "Invalid upper bound given for optimization"
 
         if not hasattr(self, "superpixel_handler"):
-            if self._lower.shape != org_img.shape:
+            if lower.shape != org_img.shape:
                 raise ValueError(
                     "Got lower bound matrix of different shape than the"
-                    f" input image. ({self._lower.shape} != {org_img.shape})"
+                    f" input image. ({lower.shape} != {org_img.shape})"
                 )
 
-            if self._upper.shape != org_img.shape:
+            if upper.shape != org_img.shape:
                 raise ValueError(
                     "Got upper bound matrix of different shape than the"
-                    f" input image.({self._upper.shape} != {org_img.shape})"
+                    f" input image.({upper.shape} != {org_img.shape})"
                 )
 
     @staticmethod
     def _get_indices_of_nonzero_entries(data: np.ndarray) -> np.ndarray:
+        """Returns the indices of the non-zero entries of a given matrix.
+
+        Args:
+            data (np.ndarray): Image data.
+
+        Returns:
+            np.ndarray: Indices of non-zero entries.
+        """
         return np.where(data > 0)[0]
-        # return np.where(data > 0)[0]
 
     def _setup_locality_measure(self):
+        """Sets up the locality measure for the perturbation distribution.
+
+        Description:
+            The locality measure is used to determine the distance between two images.
+        """
+
         def dist(x: np.ndarray, y: np.ndarray):
             # return np.linalg.norm(np.linalg.norm(z, axis=(1)), axis=(-2, -1))
-            return np.linalg.norm(x - y, axis=(-2, -1))
+            if x.ndim == 2:
+                return np.linalg.norm(x - y, axis=(-1))
+            if x.ndim == 3:
+                # axis = (-2, -1) if self.channels_first else (-2, 0)
+                return np.linalg.norm(x - y, axis=(-2, -1))
+            elif x.ndim == 4:
+                axis_1 = (-1) if self.channels_first else (1)
+                axis_2 = (-2, -1) if self.channels_first else (-2, 1)
+                return np.linalg.norm(np.linalg.norm(x - y, axis=axis_1), axis=axis_2)
+            else:
+                raise ValueError("Invalid dimensionality of input data.")
+            # axis = (-1) if x.ndim == 2 else (-2, -1)
+            # return np.linalg.norm(x - y, axis=axis)
 
         sigma = max(dist(self._stacked_org_img, self.org_img_plus_Z))
 
         # exponential kernel
         self.locality_measure = lambda x, y: np.exp(-dist(x, y) ** 2 / sigma ** 2)
 
-    def _setup_loss_constants(self):
+    def _setup_loss_constants(self) -> None:
+        """Setup constants for the loss function."""
+        # extract non-zero entries from the image
         self.non_zero_indices = LimeLoss._get_indices_of_nonzero_entries(
             self.org_img.flatten()
         )
@@ -111,7 +177,7 @@ class LimeLoss(BaseExplanationModel):
         # set of samples
         self.Z = self._draw_perturbations(self.non_zero_indices, self.n_samples)
 
-        # match org img matrix with Z matrix
+        # match org img matrix with Z matrix (perturbations matrix)
         squeezed_org_img = (
             self.org_img if self.org_img.shape[0] > 1 else self.org_img.squeeze(0)
         )
@@ -123,22 +189,35 @@ class LimeLoss(BaseExplanationModel):
 
         # flatten over 0th axis, from e.g. (2, 3, 28, 28) to (2, 3*28*28)
         self.flatten_Z = self.Z.reshape(self.Z.shape[0], np.prod(self.Z.shape[1:]))
-        self.org_img_plus_Z = (self._stacked_org_img + self.Z).clip(
-            0, self.org_img.max()
+        self.org_img_plus_Z = (
+            (self._stacked_org_img + self.Z)
+            .clip(0, self.org_img.max())
+            .astype(np.float32)
         )
 
         self._setup_locality_measure()
 
+        # compute the distance between the original image and the perturbed images
         self.Pi_Z: np.ndarray = self.locality_measure(
             self._stacked_org_img, self.org_img_plus_Z
         )
 
+        # get the prediction score of the perturbed images
         target_idx = to_numpy(self.inference(self.org_img)).argmax()
         self.pred = to_numpy(self.inference(self.org_img_plus_Z))[:, target_idx]
 
     def _draw_perturbations(
-        self, non_zero_indices: np.ndarray, n_samples
+        self, non_zero_indices: np.ndarray, n_samples: int
     ) -> np.ndarray:
+        """Draws n_samples perturbations from the perturbation distribution.
+
+        Args:
+            non_zero_indices (np.ndarray): Array containing the non-zero indices of the image.
+            n_samples (int): Number of perturbations.
+
+        Returns:
+            np.ndarray: Perturbation batch. Shape: (n_samples, *img_shape)
+        """
         return np.array(
             [
                 self._draw_one_perturbation(non_zero_indices).squeeze(0)
@@ -147,18 +226,40 @@ class LimeLoss(BaseExplanationModel):
         )
 
     def _draw_one_perturbation(self, non_zero_indices: np.ndarray) -> np.ndarray:
+        """Draws one perturbation from the perturbation distribution.
+
+        Description:
+            The perturbation is made up of 1 to k random non-zero pixels of the original image.
+
+        Args:
+            non_zero_indices (np.ndarray): Array containing the non-zero indices of the image.
+
+        Returns:
+            np.ndarray: One perturbation image.
+        """
         self.randomState = np.random.RandomState()
 
+        # draw number of pixels to include in the perturbation
         k = self.randomState.randint(low=1, high=len(non_zero_indices))
+        # draw k random non-zero pixels
         indices = np.random.choice(non_zero_indices, k, replace=False)
         indices = np.sort(indices)
 
+        # build the perturbation
         perturbation = np.zeros(self.org_img.flatten().shape, dtype=np.float64)
         values = np.take(self.org_img.flatten(), indices)
         np.put(perturbation, indices, values, mode="raise")
         return perturbation.reshape(self.org_img.shape)
 
     def get_loss(self, data: np.ndarray, *args, **kwargs) -> np.ndarray:
+        """Gets the loss for the given image model (importance weights).
+
+        Args:
+            data (np.ndarray): Image model.
+
+        Returns:
+            np.ndarray: Loss value according to LIME loss.
+        """
         return (
             1
             / self.n_samples
@@ -174,14 +275,39 @@ class SuperpixelLimeLoss(LimeLoss):
         n_samples: int,
         lower: np.ndarray = None,
         upper: np.ndarray = None,
+        channels_first: bool = False,
         superpixel_handler: SuperpixelHandler = None,
     ):
+        """Loss function for Superpixel-LIME (Local Interpretable Model-Agnostic Explanations)
+
+        For the theoretical background refer to:
+            https://arxiv.org/abs/1602.04938
+
+        Args:
+            org_img (np.ndarray): Original image [width, height, channels] or [channels, width, height].
+            inference (InferenceCall): Inference method of an external prediction entity. Has to return an \
+                interpretable representation of the underlying prediction, e.g. a binary vector indicating \
+                the “presence” or “absence”.
+            n_samples (int): Number of samples to draw from the perturbation distribution.
+            lower (np.ndarray, optional): Lower bound for the optimization. Has to be of the same shape as the \
+                target image. Defaults to None.
+            upper (np.ndarray, optional): Upper bound for the optimization. Has to be of the same shape as the \
+                target image. Defaults to None.
+            superpixel_handler (SuperpixelHandler, optional): Superpixel handler. Defaults to None.
+                
+        Configurable Parameters:
+            n_samples, channels_first
+            
+        Note:
+            The loss function is based on the LIME loss function. The only difference is that the perturbation \
+            distribution is drawn from superpixels instead of pixels.
+        """
         self.superpixel_handler = superpixel_handler
         if superpixel_handler:
             self._init_lower_upper(
                 lower, upper, self.superpixel_handler.ones_weight_vector
             )
-            self._valid_check_lower_upper(org_img)
+            self._valid_check_lower_upper(self._lower, self._upper, org_img)
 
         super().__init__(
             org_img=org_img,
@@ -190,6 +316,7 @@ class SuperpixelLimeLoss(LimeLoss):
             lower=self._lower,
             upper=self._upper,
             n_samples=n_samples,
+            channels_first=channels_first,
         )
         self._setup_loss_constants()
 
@@ -199,12 +326,19 @@ class SuperpixelLimeLoss(LimeLoss):
         upper: Union[None, np.ndarray],
         org_img: np.ndarray,
     ):
+        """Initializes the lower and upper bound for the optimization.
+
+        Args:
+            lower (Union[None, np.ndarray]): Lower bound for the optimization.
+            upper (Union[None, np.ndarray]): Upper bound for the optimization.
+            org_img (np.ndarray): Original image.
+        """
         DEFAULT_LB_UB = {
             "lower": np.full(
-                (self.superpixel_handler.num_superpixels), 0.0, np.float32
+                (self.superpixel_handler.num_superpixels), -1.0, np.float32
             ),
             "upper": np.full(
-                (self.superpixel_handler.num_superpixels), 5000.0, np.float32
+                (self.superpixel_handler.num_superpixels), 1.0, np.float32
             ),
         }
 
@@ -213,6 +347,14 @@ class SuperpixelLimeLoss(LimeLoss):
 
     @staticmethod
     def _get_indices_of_nonzero_entries(sp_label_images: np.ndarray) -> np.ndarray:
+        """Returns the indices of the non-zero entries of a label matrix.
+
+        Args:
+            data (np.ndarray): Superpixel Image.
+
+        Returns:
+            np.ndarray: Indices of non-zero entries.
+        """
         return np.array(
             [
                 i
@@ -222,6 +364,12 @@ class SuperpixelLimeLoss(LimeLoss):
         )
 
     def _setup_locality_measure(self):
+        """Sets up the locality measure for the perturbation distribution.
+
+        Description:
+            The locality measure is used to determine the distance between two images.
+        """
+
         def dist(x: np.ndarray, y: np.ndarray):
             # return np.linalg.norm(np.linalg.norm(z, axis=(1)), axis=(-2, -1))
             axis = (-1) if x.ndim == 2 else (-2, -1)
@@ -233,6 +381,8 @@ class SuperpixelLimeLoss(LimeLoss):
         self.locality_measure = lambda x, y: np.exp(-dist(x, y) ** 2 / sigma ** 2)
 
     def _setup_loss_constants(self):
+        """Setup constants for the loss function."""
+        # extract non-zero entries from the image
         self.non_zero_indices = SuperpixelLimeLoss._get_indices_of_nonzero_entries(
             self.superpixel_handler.label_images
         )
@@ -247,6 +397,7 @@ class SuperpixelLimeLoss(LimeLoss):
             [self.superpixel_handler.ones_weight_vector for _ in range(self.n_samples)]
         )
 
+        # original image has to be of shape (1, n_superpixels)
         assert self._stacked_org_img.shape == self.Z.shape
 
         # flatten over 0th axis, from e.g. (2, 3, 28, 28) to (2, 3*28*28)
@@ -261,17 +412,31 @@ class SuperpixelLimeLoss(LimeLoss):
 
         # calculate locality measure
         self._setup_locality_measure()
+        # compute the distance between the original image and the perturbed images
         self.Pi_Z: np.ndarray = self.locality_measure(
             self._stacked_org_img, self.org_img_plus_Z
         )
 
+        # get the prediction score of the perturbed images
         target_idx = to_numpy(self.inference(self.org_img)).argmax()
+        # get the images from the label weights
         org_plus_Z_images = self.superpixel_handler.generate_imgs_from_weight_vectors(
             self.org_img_plus_Z
         )
         self.pred = to_numpy(self.inference(org_plus_Z_images))[:, target_idx]
 
     def _draw_one_perturbation(self, non_zero_indices: np.ndarray) -> np.ndarray:
+        """Draws one perturbation from the perturbation distribution.
+
+        Description:
+            The perturbation is made up of 1 to k random non-zero superpixels of the superpixel image.
+
+        Args:
+            non_zero_indices (np.ndarray): Array containing the non-zero indices of the superpixel image.
+
+        Returns:
+            np.ndarray: One perturbation superpixel image.
+        """
         self.randomState = np.random.RandomState()
 
         # draw random indices from non_zero_indices
@@ -291,6 +456,14 @@ class SuperpixelLimeLoss(LimeLoss):
         # return self.superpixel_handler.generate_img_from_weight_vector(weight_vector)
 
     def get_loss(self, data: np.ndarray, *args, **kwargs) -> np.ndarray:
+        """Gets the loss for the given superpixel image model (importance weights).
+
+        Args:
+            data (np.ndarray): Image model.
+
+        Returns:
+            np.ndarray: Loss value according to LIME loss.
+        """
         return (
             1
             / self.n_samples
