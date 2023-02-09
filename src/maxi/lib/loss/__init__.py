e7e198a4a803ff872956d6da3341f3d17af5285c
tuananhroman <anh.maxiking@hotmail.de>
2023-02-09 18:54:08 +0100

    bug fix

--- src/maxi/lib/loss/__init__.py
+++ src/maxi/lib/loss/__init__.py
@@ -1,7 +1,7 @@
 """Explanation Models (Loss Functions)"""
 from .base_explanation_model import BaseExplanationModel
 from .cem_loss import CEMLoss
-from .lime_loss import LimeLoss, SuperpixelLimeLoss
+from .lime_loss import LimeLoss
 from .tf_cem_loss import TF_CEMLoss
 from .torch_cem_loss import Torch_CEMLoss
 from .segmentation_loss import *
